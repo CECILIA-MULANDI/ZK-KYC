63d5c4b970d84e6c2ea5e71a5a6c8c9fe75befca
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2024-12-11 13:48:07 +0300

    basic logic

---
+++ zkkyc/lib/src/lib.rs
@@ -0,0 +1,29 @@
+// use alloy_sol_types::sol;
+
+// sol! {
+//     /// The public values encoded as a struct that can be easily deserialized inside Solidity.
+//     struct PublicValuesStruct {
+//         uint32 n;
+//         uint32 a;
+//         uint32 b;
+//     }
+// }
+// for the proof of concept, we take in a string as the doc<input>
+
+use std::io;
+
+pub fn submit_docs(mut doc: u32) -> bool {
+    // assume we are submitting an id
+    // parse the doc
+    // calculate age
+    // if > 18 then legal age
+    // let mut input = String::new();
+    // io::stdin()
+    //     .read_line(&mut input)
+    //     .expect("Could not read the file");
+    // doc = input
+    //     .trim()
+    //     .parse()
+    //     .expect("The value passed should be an integer");
+    doc > 18
+}

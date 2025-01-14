d22822428fd758b01e1cc0b9ad2db07b884f6c8c
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-01-14 14:10:25 +0300

    creation of user documents

--- zkkyc/proofsystems/program/src/main.rs
+++ zkkyc/proofsystems/program/src/main.rs
@@ -6,10 +6,18 @@ use fibonacci_lib::{KycUpload,new}
 pub fn main() {
     // Read an input to the program.
 
-    let doc = sp1_zkvm::io::read::<u32>();
-    sp1_zkvm::io::commit(&doc);
-    // pass in the doc to the function from the lib.rs
-    let res = submit_docs(doc);
+    let name: = sp1_zkvm::io::read::<String>();
+    let dob: = sp1_zkvm::io::read::<String >();
+    let gender: = sp1_zkvm::io::read::< Option<String>>();
+    let signature: = sp1_zkvm::io::read::<String>();
+    let address: = sp1_zkvm::io::read::<String>();
+    let id_number:  = sp1_zkvm::io::read::<String>();
+    let id_type:  = sp1_zkvm::io::read::<String>();
+    let issue_date:= sp1_zkvm::io::read::<String >();
+    let expiry_date: = sp1_zkvm::io::read::<Option<String>>();
+   let doc =KycUpload::new(
+    name,dob,gender,signature,address,id_number,id_type,issue_date,expiry_date
+   );
 
-    sp1_zkvm::io::commit(&res);
+    sp1_zkvm::io::commit(&doc);
 }
